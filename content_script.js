fe4338facd3d739d823eef1c80e718567de0f654
ilyas Turkben <yesilturk@gmail.com>
2012-06-05 11:18:04 +0300

    Update master

--- content_script.js
+++ content_script.js
@@ -1,11 +1,8 @@
 var head = document.getElementsByTagName("head").item(0);
 var style = document.createElement("style");
-head.appendChild(style);
-var styles = document.getElementsByTagName("style");
-style = styles.item(styles.length-1);
 style.innerText+="#pagelet_ego_pane_w .ego_column {display:none;}";
 style.innerText+="#pagelet_ego_pane .ego_column {display:none;}";
 style.innerText+="#pagelet_ego_pane .ego_column {display:none;}";
 style.innerText+=".fbTimelineSideAds .ego_column {display:none;}";
 style.innerText+=".egoOrganicColumn {display:block !important;}";
-
+head.appendChild(style);
